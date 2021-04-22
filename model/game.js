a1dc9367d441872b18cc06435d330397883be7d7
Fisher Huang <810881@gs.hs.ntnu.edu.tw>
2021-04-22 19:11:49 +0800

    update mongoose update to localhost

--- model/game.js
+++ model/game.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose")
-mongoose.connect(
-  "mongodb+srv://hsnubbc506:hsnubbc506@cluster0.ykmvh.mongodb.net/hsnubbc506?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
-)
+if (process.env.NODE_ENV === "production") {
+  mongoose.connect(
+    "mongodb+srv://hsnubbc506:hsnubbc506@cluster0.ykmvh.mongodb.net/hsnubbc506?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+} else {
+  mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true })
+}
 
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "connection error:"))
